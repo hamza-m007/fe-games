ce400f70306d6579e275251726ec4f8c14e8444e
hamza <hamza.m2113@gmail.com>
2022-12-05 20:48:20 +0000

    header added

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import { useContext } from "react";
+// import { userContext } from "react"
+
+export default function Header () {
+    return (
+        <header>
+            <hi>{'Game Board Reviews'}</hi>
+        </header>
+    );
+}
